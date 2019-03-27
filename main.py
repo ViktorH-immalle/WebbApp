9129f0475f4c37e5e10eacd9220e2e0798fd65c5
Hendrickx Viktor <viktor.hendrickx@immalle.eu>
2019-03-27 10:31:15 +0100

    Create Family

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import *
+from flask import Flask, redirect, render_template, request, url_for
 from flask_login import login_required, LoginManager, UserMixin, login_user, logout_user
 from sqlite3 import connect, Cursor
 
@@ -16,13 +16,13 @@ class User(UserMixin):
 def load_user(user_id):
     return User(user_id)
 
-@app.route('/Login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         db = connect('database')
         cur = db.cursor()
-        formUsername = request.form['Username']
-        formPassword = request.form['Password']
+        formUsername = request.form['username']
+        formPassword = request.form['password']
         cur.execute('SELECT * FROM user WHERE username ="' + formUsername + '" AND Password ="' + formPassword + '";')
         data = cur.fetchall()
         if len(data) == 0:
@@ -31,28 +31,29 @@ def login():
             cur.execute('SELECT userid FROM user WHERE username ="' + formUsername + '" AND Password ="' + formPassword + '";')
             userid = cur.fetchone()[0]
             login_user(User(userid))
-            return 'Welcome'
+            return render_template('Agenda.html')
+
     return render_template('Login.html')
 
-@app.route('/Agenda')
+@app.route('/agenda')
 @login_required
 def agenda():
     return render_template('Agenda.html')
 
-@app.route('/Logout')
+@app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route('/CreateUser', methods=['GET','POST'])
+@app.route('/createuser', methods=['GET','POST'])
 def createuser():
     if request.method == 'POST':
         database = connect('database')
         cur = database.cursor()
-        email = request.form['Email']
-        username = request.form['Username']
-        password = request.form['Password']
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
 
         cur.execute('INSERT OR IGNORE INTO user (email, username, password) VALUES("' + email + '","' + username + '","' + password + '");')
         database.commit()
@@ -62,6 +63,23 @@ def createuser():
         else:    
             return redirect(url_for('login'))
     return render_template('CreateUser.html')
+
+@app.route('/createfamily', methods=['GET', 'POST'])
+@login_required
+def createfamily():
+    if request.method == 'POST':
+        database = connect('database')
+        cur = database.cursor()
+        familyname = request.form['familyname']
+
+        cur.execute('INSERT OR IGNORE INTO family (familyname) VALUES("' + familyname +'");')
+        database.commit()
+        if cur.lastrowid == 0:
+            return redirect(url_for('createfamily'))
+        else:    
+            return redirect(url_for('agenda'))
+        return render_template('Agenda.html')
+    return render_template('CreateFamily.html')
 
 if __name__ == "__main__":
     app.secret_key = "123"
